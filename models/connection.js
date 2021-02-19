2dc8072faca9c3e471c1ae977ce5bb3b5086b964
cédric <cedric_lecalvez@hotmail.com>
2021-02-19 17:15:17 +0100

    changement version jest fait

--- models/connection.js
+++ models/connection.js
@@ -3,14 +3,18 @@ var mongoose = require('mongoose');
 var options = {
     connectTimeoutMS: 5000,
     useNewUrlParser: true,
-   
-        useUnifiedTopology : true
+    useUnifiedTopology : true
    }
+
    mongoose.connect(process.env.MONGODB,
-    options,    
-    function(err) {
-     console.log(err);
-    }
+        options,    
+        function(err) {
+            if (err){
+                console.log(err);
+            } else {
+                console.log('conection BD ok');
+            }
+        }
    );
 
    module.exports = mongoose
