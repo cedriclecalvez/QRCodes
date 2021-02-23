d8abea378ab78f33904979eff75c0b3ecd81ccec
Viacheslav ZAITCEV <zaitcev@mail.fr>
2021-02-23 11:13:11 +0100

    connexion API par request

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const fs = require('fs');
-const http = require('https');
 const request = require('request')
-// var unirest = require("unirest");
+
   
 
 
@@ -38,7 +37,7 @@ const options = {
     formData: {
       image: {
         value: fs.createReadStream('./public/img/logo192.png'),
-        options: {filename: '1.JPG', contentType: 'application/octet-stream'}
+        options: {filename: 'logo192.png', contentType: 'application/octet-stream'}
       },
       text: req.body.data
     }
@@ -53,84 +52,6 @@ const options = {
 
 
 
-
-
-    // HTTP REQUEST
-    /* 
-
-    const options = {
-        "method": "POST",
-        "hostname": "qrcode3.p.rapidapi.com",
-        "port": null,
-        "path": "/generateQRwithLogo",
-        "headers": {
-            "content-type": "multipart/form-data; boundary=---011000010111000001101001",
-            'x-rapidapi-key': process.env.RAPIDAPI,
-            'x-rapidapi-host': 'qrcode3.p.rapidapi.com',
-            useQueryString: true
-        },
-        formData
-    }
-
-    const responseAPI = await http.request (options, function (res) {
-        const chunks = [];
-    
-        res.on("data", function (chunk) {
-            chunks.push(chunk);
-        });
-    
-        res.on("end", function () {
-            const body = Buffer.concat(chunks);
-            console.log('body=', body)
-        });
-    });
-
-    console.log('reponse API=',responseAPI);
-
-    // for (let arg in responseAPI){
-    //     console.log('arg=', arg)
-    // }
-    
-    let result2 = {};
-    result.HTTPParser=responseAPI.connection.parser;
-
-    // console.log('responseAPI.connection.parser=', responseAPI.connection.parser)
-*/
-
-
-
-
-//  request with unirest
-/*
-    var reqAPI = unirest("POST", "https://qrcode3.p.rapidapi.com/generateQRwithLogo");
-    
-    reqAPI.headers({
-        "content-type": "multipart/form-data; boundary=---011000010111000001101001",
-        "x-rapidapi-key": "86e262822emshf8f1554c03d58f5p1c48ebjsn62553c054c54",
-        "x-rapidapi-host": "qrcode3.p.rapidapi.com",
-        "useQueryString": true
-    });
-    
-    reqAPI.multipart([
-        {
-            image: {},
-            "text": req.body.data
-        }
-    ]);
-    
-    
-    reqAPI.end(function (resAPI) {
-        if (resAPI.error) {
-            console.log('resAPI.error=', resAPI.error);
-        }else{
-
-            console.log('reponse API=',resAPI.body);
-
-            result.responseAPI=resAPI.body;
-        }
-
-    });
-*/
     console.log('result=', result)
     res.json(result)
 })
